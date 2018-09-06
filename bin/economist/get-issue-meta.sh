7748a5c66e62e6b5e1cfa13cad49ae07b6265902
DeftSEO <deftseo@inboxlite.com>
2018-09-06 12:37:14 -0400

    Skeleton of script to iteratively get issue meta/table of contents.

---
+++ bin/economist/get-issue-meta.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+CWD=`pwd`
+BIN_DIR="${CWD}/bin/economist/"
+DATA_DIR="data/economist/"
+META_FILE="index.json"
+
+YEAR=$1
+YEAR_DIR="${DATA_DIR}${YEAR}"
+
+# echo $YEAR_DIR
+# echo $BIN_DIR
+
+cd $YEAR_DIR
+
+for ISSUE_DIR in */; do
+    # echo "$ISSUE_DIR"
+    ISSUE_FILE="${ISSUE_DIR}${META_FILE}"
+    
+    if [ ! -f $ISSUE_FILE ]; then
+        ISSUE_DATE="${ISSUE_DIR::-1}"
+        echo "Creating: ${ISSUE_DATE}"
+    fi
+done;
+
+cd $CWD
+
+
