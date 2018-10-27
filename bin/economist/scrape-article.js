e419cc1123d8e0bc439f5f2007dbb2e51cd174e7
DeftSEO <deftseo@inboxlite.com>
2018-10-27 08:43:55 -0400

    Clean up scrape init.

--- bin/economist/scrape-article.js
+++ bin/economist/scrape-article.js
@@ -1,10 +1,10 @@
-var Scraper = require('node-crawler/Scraper');
+var DeftCrawler = require('deft-crawler');
 var Economist = require('../../src/sources/Economist');
 
 var args = process.argv.slice(2);
 var articleUrl = (args.length) ? args[0] : "https://www.economist.com/node/21749064";
 
-Scraper.Scraper(articleUrl, function($page, pageUrl) {
+DeftCrawler.scrape(articleUrl, function($page, pageUrl) {
     var article = Economist.getArticle($page, pageUrl);
     console.log(JSON.stringify(article, null, 4));
 });
